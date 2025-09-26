3be7f52b35746f5fa0e54b8604618f194e54ca16
MaximusMikaelson <maxim.snezhnitskiy@gmail.com>
2025-09-26 12:29:57 +0300

    3 - приостановка проекта

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
 
+if (! function_exists('maxim_snezhnitskiy_setup')) :
+    function maxim_snezhnitskiy_setup() {
+        // добавляем пользовательский логотип
+        add_theme_support( 'custom-logo', array(
+            'height'      => 50,
+            'width'       => 130,
+            'flex-height' => false,
+            'flex-width'  => false,
+            'header-text' => '',
+        ) );
+        // добавляем динамический тег <title>
+        add_theme_support( 'title-tag' );
+    }
+    add_action( 'after_setup_theme', 'maxim_snezhnitskiy_setup' );
+endif;
+
 // Подключение скриптов и стилей
 add_action( 'wp_enqueue_scripts', 'maxim_snezhnitskiy_scripts' );
 
@@ -9,8 +25,8 @@ function maxim_snezhnitskiy_scripts() {
     // --- Стили ---
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/plugins/bootstrap/css/bootstrap.css', array(), '4.6.0' );
     wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/plugins/fontawesome/css/all.css', array('bootstrap'), '6.4.0' );
-    wp_enqueue_style( 'animate', get_template_directory_uri() . '/plugins/animate-css/animate.css', array('bootstrap'), '4.1.1' );
-    wp_enqueue_style( 'icofont', get_template_directory_uri() . '/plugins/icofont/icofont.css', array('bootstrap'), '1.0.1' );
+    wp_enqueue_style( 'animate', get_template_directory_uri() . '/plugins/animate-css/animate.css', array('bootstrap'), '1.0.0');
+    wp_enqueue_style( 'icofont', get_template_directory_uri() . '/plugins/icofont/icofont.css', array('bootstrap'), '1.0.0' );
     
     wp_enqueue_style( 'main-style', get_stylesheet_uri(), array('bootstrap', 'fontawesome', 'animate', 'icofont'), $theme_version );
     wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/css/style.css', array('main-style'), filemtime(get_template_directory() . '/css/style.css') );
@@ -28,5 +44,27 @@ function maxim_snezhnitskiy_scripts() {
     wp_enqueue_script( 'easing', get_template_directory_uri() . '/plugins/easing/easing.min.js', array('jquery'), '1.0', true );
     wp_enqueue_script( 'contact', get_template_directory_uri() . '/plugins/jquery/contact.js', array('jquery'), '1.0', true );
     wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), filemtime(get_template_directory() . '/js/custom.js'), true );
+}
+
+/* 
+ * Регистрируем области меню
+ */
+function maxim_snezhnitskiy_menus() {
+    // собираем массив областей меню
+	$locations = array(
+		'header'  => __( 'Header Menu', 'maxim_snezhnitskiy' ),
+		'footer'   => __( 'Footer Menu', 'maxim_snezhnitskiy' ),
+	);
+    // регистрируем области меню
+	register_nav_menus( $locations );
+}
+// хук для регистрации областей меню
+add_action( 'init', 'maxim_snezhnitskiy_menus' );
+// добавляем класс navbar-item ко всем элементам списка меню
+add_filter( 'nav_menu_css_class', 'custom_nav_css_class', 10, 1 );
+// функция добавления класса
+function custom_nav_css_class( $classes ) {
+    $classes[] = 'nav-item';
+    return $classes;
 }
 ?>
---
+++ header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="seo & digital marketing" />
+    <meta
+        name="keywords"
+        content="marketing,digital marketing,creative, agency, startup,promodise,onepage, clean, modern,seo,business, company"
+    />
+
+    <?php wp_head(); ?>
+
+    <!-- Icofont -->
+    <link rel="stylesheet" href="plugins/icofont/icofont.css" />
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body data-spy="scroll" data-target=".fixed-top">
+    <nav class="navbar navbar-expand-lg fixed-top trans-navigation">
+      <div class="container">
+        <?php
+            if ( has_custom_logo() ) {
+                echo get_custom_logo();
+            }
+            else { }
+        ?>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mainNav"
+            aria-controls="mainNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon">
+            <i class="fa fa-bars"></i>
+          </span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="mainNav">
+            <?php wp_nav_menu( array(
+                'theme_location' => 'header',
+                'container' => false,
+                'menu_class' => 'navbar-nav',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            ) ); ?>
+            <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/"> Главная </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarWelcome"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+                >О нас</a
+              >
+              <div class="dropdown-menu" aria-labelledby="navbarWelcome">
+                <a class="dropdown-item" href="about.html"> О компании </a>
+                <a class="dropdown-item" href="about.html"> Об услугах </a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link smooth-scroll" href="service.html">Услуги</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link smooth-scroll" href="pricing.html">Цены</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link smooth-scroll" href="blog.html">Журнал</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link smooth-scroll" href="contact.html">Контакты</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!--MAIN HEADER AREA END -->
